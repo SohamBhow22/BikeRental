a40000690f77bccb7f749480b3f80c9db92e4d51
Soham Bhowmick <32588339+SohamBhow22@users.noreply.github.com>
2019-04-25 12:43:37 +0530

    Add files via upload

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,24 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Brands</a>
+<ul>
+<li><a href="create-brand.php">Enter a new Brand</a></li>
+<li><a href="manage-brands.php">Manage Brands</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Bikes</a>
+					<ul>
+						<li><a href="post-avehical.php">Enter a new Bike</a></li>
+						<li><a href="manage-vehicles.php">Manage Bikes</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Manage Booking</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
+			</ul>
+		</nav>
